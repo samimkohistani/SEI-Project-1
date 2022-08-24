dda7a2b27323ae683443a3ce10375174368e75a8
Samim Kohistani <samimkohistani@samims-air.myfiosgateway.com>
2022-08-24 17:33:53 -0400

    know your cats

--- jss/script.js
+++ jss/script.js
@@ -1,10 +1,17 @@
-let button = document.getElementById('button')
-const $inpValue = $('#input')
-const $origin = $('#origin')
-const $length = $('#length')
-const $intelligence = $('#intelligence')
-const $playfulness = $ ('#playfulness')
-const $other_pets_friendly = $('#other_pets_friendly')
+let button = document.getElementById('button');
+const $inpValue = $('#input');
+const $origin = $('#origin');
+const $length = $('#length');
+const $intelligence = $('#intelligence');
+const $playfulness = $ ('#playfulness');
+const $other_pets_friendly = $('#other_pets_friendly');
+const $image_link = $('#image_link');
+const $min_weight = $('#min_weight');
+const $max_weight = $('#max_weight');
+const $min_life_expectancy = $('#min_life_expectancy');
+const $max_life_expectancy = $('#max_life_expectancy');
+const $family_friendly = $('#family_friendly');
+
 
 let catData;
 button.addEventListener('click' , function(){
@@ -23,6 +30,13 @@ button.addEventListener('click' , function(){
     $intelligence.text(catData.intelligence);
     $playfulness.text(catData.playfulness);
     $other_pets_friendly.text(catData.other_pets_friendly);
+    $min_weight.text(catData.min_weight);
+    $max_weight.text(catData.max_weight);
+    $min_life_expectancy.text(catData.min_life_expectancy);
+    $max_life_expectancy.text(catData.max_life_expectancy);
+    $family_friendly.text(catData.family_friendly);
+    $image_link.text(catData.image_link);
+
 
 },
     
